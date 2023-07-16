341ae4a7997972b6107c7a7c9ac8d0ad12b5bb05
Nane Pothier <nane.pothier@gmail.com>
2023-07-16 16:06:26 +0200

    Add test file for AllPlanInfo component

--- src/components/Welcome/AllPlanInfo.js
+++ src/components/Welcome/AllPlanInfo.js
@@ -9,7 +9,10 @@ const AllPlanInfo = () => {
         <div className={`${classes.heading2} ${classes.text}`}>
           All The TV You Love
         </div>
-        <div className={`${classes.heading3} ${classes.text}`}>
+        <div
+          className={`${classes.heading3} ${classes.text}`}
+          data-testid={"heading3"}
+        >
           Stream full seasons of exclusive series, current-season episodes, hit
           movies, BumbleBee Originals, kids shows and more.
         </div>
